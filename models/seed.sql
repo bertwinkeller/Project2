00785c467491666a7985c204ccf6eded3b049543
Michael Dunsterville <michael@klientboost.com>
2019-11-26 19:35:03 -0800

    Added images and Seed File

---
+++ models/seed.sql
@@ -0,0 +1,13 @@
+INSERT INTO vehicles (name, reservedFrom, mpg, gasType, doors, seats, photoLink, createdAt, updatedAt) VALUES 
+('Ferrari F430 F1', '{}', 11, 'Premium', 2, 2, './images/Ferrari_F430_F1.jpg', '2019-11-26 10:01:00', '2019-11-26 10:01:00'),
+('Ferrari 458 ITALIA', '{}', 13, 'Premium', 2, 2, './images/Ferrari_458_ITALIA.jpg', '2019-11-26 10:01:00', '2019-11-26 10:01:00'),
+('Ferrari 488 GTB', '{}', 15, 'Premium', 2, 2, './images/Ferrari_488_GTB.jpeg', '2019-11-26 10:01:00', '2019-11-26 10:01:00'),
+('Lamborghini GALLARDO LP550-2', '{}', 13, 'Premium', 2, 2, './images/Lamborghini_GALLARDO_LP550-2.jpg', '2019-11-26 10:01:00', '2019-11-26 10:01:00'),
+('Lamborghini HURACAN LP580-2', '{}', 13, 'Premium', 2, 2, './images/Lamborghini_HURACAN_LP580-2.jpg', '2019-11-26 10:01:00', '2019-11-26 10:01:00'),
+('McLaren 570S', '{}', 16, 'Premium', 2, 2, './images/McLaren_570S.jpg', '2019-11-26 10:01:00', '2019-11-26 10:01:00'),
+('Aston Martin DB11', '{}', 18, 'Premium', 2, 4, './images/Aston_Martin_DB11.jpg', '2019-11-26 10:01:00', '2019-11-26 10:01:00'),
+('Mercedes-Benz AMG GT', '{}', 19, 'Premium', 2, 2, './images/Mercedes-Benz_AMG_GT.jpg', '2019-11-26 10:01:00', '2019-11-26 10:01:00'),
+('BMW i8', '{}', 0, 'Electric', 2, 4, './images/BMW_i8.jpg', '2019-11-26 10:01:00', '2019-11-26 10:01:00'),
+('Acurra NSX', '{}', 21, 'Hybrid/Premium', 2, 4, './images/Acura_NSX.jpg', '2019-11-26 10:01:00', '2019-11-26 10:01:00'),
+('Alfa Romeo 4C Spider', '{}', 24, 'Premium', 2, 2, './images/Alfa_Romeo_4C_Spider.png', '2019-11-26 10:01:00', '2019-11-26 10:01:00'),
+('Audi S8', '{}', 14, 'Premium', 4, 5, './images/Audi_S8.jpg', '2019-11-26 10:01:00', '2019-11-26 10:01:00');
