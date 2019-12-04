6d81be609b5e44a5f8c22cf325d999860c3d51b5
Michael Dunsterville <michael@klientboost.com>
2019-12-03 18:49:11 -0800

    Removed Post multiple

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const sequelize = require('../config')
 
 const Vehicle = require('./Vehicle.js')(Model, DataTypes, sequelize)
 const Review = require('./Review.js')(Model, DataTypes, sequelize)
-// const User = require('./User.js')(Model, DataTypes, sequelize)
+const User = require('./User.js')(Model, DataTypes, sequelize)
 
 Vehicle.hasMany(Review)
 Review.belongsTo(Vehicle)
