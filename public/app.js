cb75df70a222b77b05d80b9cf98cba0a5f728f49
Bert Keller <bertwinkeller@gmail.com>
2019-12-07 11:23:37 -0800

    changed the border

--- public/app.js
+++ public/app.js
@@ -45,7 +45,7 @@ const displayVehicles = (array) => {
         console.log(car)
         let carCard = document.createElement('div')
         carCard.innerHTML = `
-        <div class="card text-center border-0 mb-3">
+        <div class="card text-center border-1 mb-3">
           <div class="card-body">
             <img class="img-fluid" src="${car.photoLink}">
             <div class="row">
